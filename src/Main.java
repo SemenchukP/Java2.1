070b31e154db177c8f9c154a52e6c63eeef44713
SemenchukP <semenchuk.p79@gmail.com>
2022-07-21 21:36:29 +0500

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int price = 25_000;
+        int rubles = 20;
+        int miles = price / rubles;
+        System.out.println("Начислено:" + miles +  "миль");
+    }
+}
